2c52d6d300ddad8c7e6ba982461bc735089b06d8
singhviyashvi <yashvisinghvi0109@gmail.com>
2026-02-13 11:30:58 +0530

    resolved merge conflict

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center text-4xl font-bold">
+      Tailwind v4 is alive.
+    </div>
+  )
+}
+
+export default App
+
